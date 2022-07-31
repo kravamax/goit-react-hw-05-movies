03176e05ab67ba0ecf829a523302f263664d8769
kravamax <32734778+kravamax@users.noreply.github.com>
2022-07-31 15:14:42 +0000

    Deploying to gh-pages from @ kravamax/goit-react-hw-05-movies@a81cafe0195c3bc16cbbf45aabe9a98f8100544e 🚀

--- static/js/174.fdc1f5c2.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[174],{174:function(e,t,r){r.r(t),r.d(t,{default:function(){return i}});var n=r(501),a=r(871),c=r(184),i=function(e){var t=e.movies,r=(0,a.TH)();return(0,c.jsx)("ul",{children:t.map((function(e){var t=e.title,a=e.name,i=e.id;return(0,c.jsx)("li",{children:(0,c.jsx)(n.OL,{to:"/movies/".concat(i),state:{from:r},children:t||a})},i)}))})}}}]);
-//# sourceMappingURL=174.fdc1f5c2.chunk.js.map
